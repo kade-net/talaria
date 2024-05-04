b529216a5a1c8a149a400dbbd85131804db18c76
porkytheblack <porkytheblack@gmail.com>
2024-05-04 11:40:59 +0300

    feat: added inbox search

--- apps/api/src/resolvers/queries/index.ts
+++ apps/api/src/resolvers/queries/index.ts
@@ -8,6 +8,7 @@ interface ResolverMap {
         phoneBook: Resolver<never, { address: string }, never>
         inboxHistory: Resolver<never, PaginationArg & { inbox_name: string, sort: SORT_ORDER, timestamp: number }, never>
         inboxes: Resolver<never, PaginationArg & { address: string, sort: SORT_ORDER, active: boolean, type: INBOX_TYPE }, never>
+        inbox: Resolver<never, { viewer: string, address: string, active?: boolean }, never>
     },
     PhoneBook: {
         contacts: Resolver<PHONEBOOK, never, never>
@@ -72,6 +73,25 @@ export const hermesQueries: ResolverMap = {
             })
 
             return inboxes ?? []
+        },
+        inbox: async (_, args, __) => {
+            const active = args.active == false ? false : args.active
+            const inbox = await db.query.inbox.findFirst({
+                where(fields, ops) {
+                    return ops.and(ops.or(
+                        ops.and(
+                            ops.eq(fields.owner_address, args.viewer),
+                            ops.eq(fields.initiator_address, args.address)
+                        ),
+                        ops.and(
+                            ops.eq(fields.owner_address, args.address),
+                            ops.eq(fields.initiator_address, args.viewer)
+                        )
+                    ),
+                        active ? ops.eq(fields.active, active) : undefined
+                    )
+                }
+            })
         }
     },
     PhoneBook: {
